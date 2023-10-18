8a55ca4f558e0b97d18958f638dc14208420bb19
今村エリカ <k023g1045@g.neec.ac.jp>
2023-10-18 10:44:12 +0900

    プルしません追加

--- git_test.cpp
+++ git_test.cpp
@@ -8,6 +8,7 @@ int main()
     std::cout << "Hello Git Hub!\n";
     std::cout << "yaho\n";
     std::cout << "pull request";
+    std::cout << "プルしません";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
