97afa0e9c3e0ed7f72f9ae36d0a7918abf662aac
今村エリカ <k023g1045@g.neec.ac.jp>
2023-12-06 10:49:52 +0900

    aaa

--- git_test.cpp
+++ git_test.cpp
@@ -6,8 +6,11 @@
 int main()
 {
     std::cout << "Hello Git Hub!\n";
-
-    std::cout << "Clone changed\n";
+    std::cout << "yaho\n";
+    std::cout << "pull request";
+  
+    std::cout << "プルしてください～";
+    std::cout << "プルしません";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
